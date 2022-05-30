d919bccb691c8226189d63f322b5e985a3ebcfb9
Matt Watson <mwatson@renci.org>
2022-05-29 22:10:09 -0400

    add cssbaseline

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import { App } from './app'
 import { createRoot } from 'react-dom/client'
 import { GraphProvider } from './context'
-import './index.css'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import { theme } from './theme'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 
 const ProvisionedApp = () => {
   return (
-    <GraphProvider>
-      <App />
-    </GraphProvider>
+    <ThemeProvider theme={ theme }>
+      <GraphProvider>
+        <CssBaseline />
+        <App />
+      </GraphProvider>
+    </ThemeProvider>
   )
 }
 root.render(<ProvisionedApp />)
