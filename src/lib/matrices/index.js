48344db806e7e3d896964058063489e2aaf36dc3
Matt Watson <mwatson@renci.org>
2022-06-03 21:12:05 -0400

    add Cell600

--- src/lib/matrices/index.js
+++ src/lib/matrices/index.js
@@ -2,6 +2,7 @@ import { Balaban11Cage } from './balaban11cage'
 import { BidiakisCube } from './bidiakis-cube'
 import { BuckyBall } from './buckyball'
 import { BullGraph } from './bull-graph'
+import { Cell600 } from './cell-600'
 import { GolombGraph } from './golomb-graph'
 import { GossetGraph } from './gosset-graph'
 import { McGeeGraph } from './mcgee-graph'
@@ -11,6 +12,7 @@ export default {
   BidiakisCube,
   BuckyBall,
   BullGraph,
+  Cell600,
   GolombGraph,
   GossetGraph,
   McGeeGraph,
